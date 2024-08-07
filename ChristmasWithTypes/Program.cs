0db574fd217e8d9968b390d9454b29f9cdd3b798
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-07 15:51:51 -0400

    third commit, trying to fix line 11

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -8,7 +8,7 @@ namespace ChristmasWithTypes
         {
             var xmas = new Christmas();
 
-            var xmasDay = Christmas.Day.Thursday;
+            var xmasDay = Christmas.Day.Wednesday;
 
             //TODO set Santa's name to Kris Kringle
             xmas.Santa = "Kris Kringle";
